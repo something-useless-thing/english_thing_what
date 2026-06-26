75b1ce472e3e2e2d8de2bc3f2d6864cda61436d1
HEEWON0010 <chw001025@gmail.com>
2026-06-26 11:12:19 +0900

    fix: import paths lowercase to match actual filenames

--- src/main.js
+++ src/main.js
@@ -2,10 +2,10 @@ import './style.css'
 import { appStore } from './stores/store.js'
 import { renderMain } from './pages/Main.js'
 import { renderWord } from './pages/Word.js'
-import { renderPassage } from './pages/Passage.js'
+import { renderPassage } from './pages/passage.js'
 import { renderGrammar } from './pages/Grammar.js'
-import { renderConcept } from './pages/Concept.js'
-import { renderRanking } from './pages/Ranking.js'
+import { renderConcept } from './pages/concept.js'
+import { renderRanking } from './pages/ranking.js'
 import { renderComprehensive } from './pages/Comprehensive.js'
 
 const app = document.getElementById('app')
